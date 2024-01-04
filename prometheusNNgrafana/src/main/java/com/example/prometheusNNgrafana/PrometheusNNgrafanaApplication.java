c24d01a9f505ee00a6c8cbcff79476072b121254
Blesson <blessonxavierofficial@gmail.com>
2024-01-04 20:02:09 +0530

    First Commit

---
+++ prometheusNNgrafana/src/main/java/com/example/prometheusNNgrafana/PrometheusNNgrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prometheusNNgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusNNgrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusNNgrafanaApplication.class, args);
+	}
+
+}
---
+++ prometheusNNgrafana/src/test/java/com/example/prometheusNNgrafana/PrometheusNNgrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometheusNNgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusNNgrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
